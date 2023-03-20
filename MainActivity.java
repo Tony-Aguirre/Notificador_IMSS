9ca6cba197a8fd9f02a93a09e92027e72537cb1e
Tony-Aguirre <128250985+Tony-Aguirre@users.noreply.github.com>
2023-03-20 12:42:17 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,24 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText Usuario,Password;
-    EditText UsuarioBD,PasswordBD;
     RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,42 +26,33 @@ public class MainActivity extends AppCompatActivity {
         Usuario = (EditText)findViewById(R.id.txtUsuario);
         Password = (EditText)findViewById(R.id.txtPassword);
     }
-    //Método botón Iniciar Sesión
     public void IniciarSesion(View view){
-        String usuario = Usuario.getText().toString();
-        String password = Password.getText().toString();
-        if(usuario.length() == 0){
-            Toast.makeText(this, "Debes ingresar el usuario", Toast.LENGTH_SHORT).show();
-        }if(password.length() == 0){
-            Toast.makeText(this, "Debes ingresar la contraseña", Toast.LENGTH_SHORT).show();
-        }if(usuario.length() != 0 && password.length() != 0){
-            String URL="http://192.168.0.10:80/dashboard/conexion/buscar_notificador.php?matricula="+usuario+"";
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, (response) -> {
-                JSONObject jsonObject = null;
-                for (int i=0; i < response.length(); i++) {
-                    try {
-                        jsonObject = response.getJSONObject(i);
-                        UsuarioBD.setText(jsonObject.getString("matricula"));
-                        PasswordBD.setText(jsonObject.getString("password"));
-                    } catch (JSONException e) {
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(),"Error de Conexión",Toast.LENGTH_SHORT).show();
+        String URL = "http://192.168.0.10/conexion/validar_usuario.php";
+        StringRequest stringRequest =  new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    Intent intent = new Intent (getApplicationContext(),Pendientes.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"Usuario o contraseña incorrectos",Toast.LENGTH_SHORT).show();
                 }
             }
-            );
-            requestQueue=Volley.newRequestQueue(this);
-            requestQueue.add(jsonArrayRequest);
-            String CompUsuario = UsuarioBD.getText().toString();
-            String CompPassword = PasswordBD.getText().toString();
-            if(CompUsuario.equals(usuario) && CompPassword.equals(password)){
-                Intent siguiente = new Intent (MainActivity.this, Pendientes.class);
-                startActivity(siguiente);
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_SHORT).show();
             }
-        }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String,String>();
+                parametros.put("matricula",Usuario.getText().toString());
+                parametros.put("password",Password.getText().toString());
+                return parametros;
+            }
+        };
+        requestQueue=Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
