60faec0bb5f5abf5d5d5e84f91815e7da27861a3
Tony-Aguirre <128250985+Tony-Aguirre@users.noreply.github.com>
2023-03-20 12:40:29 -0700

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,6 @@
 <?php
     try{
         $conexion = mysqli_connect("localhost","root","","notificador_imss");
-        if($conexion){
-            echo "<script> alert('Conexion exitosa.');</script>";
-        }
     }catch(\Exeption $e){
         throw $e;
     }
